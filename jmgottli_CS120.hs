5289e257db5916ae6532f20c9473d1e35b3e5d1c
jordanvtskier1 <112504589+jordanvtskier1@users.noreply.github.com>
2022-10-25 13:51:32 -0400

    Update jmgottli_CS120.hs

--- jmgottli_CS120.hs
+++ jmgottli_CS120.hs
@@ -1,9 +1,5 @@
-let numbers = [1,2,3]
-let letters = ['A','B','C']
-print numbers!!0
-print letters!!0
-print numbers!!1
-print letters!!1
-print numbers!!2
-print letters!!2
+main = do
+    let numbers = [1,2,3]
+    let letters = ['A','1','B','2','C','3']
+    putStrLn letters
 
